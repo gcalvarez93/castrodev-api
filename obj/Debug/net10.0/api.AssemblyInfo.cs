6389a99f1e2ba9c2f2e0833d6fcbfee8bc766063
Gabriel Castro <gcalvarez93@gmail.com>
2026-06-08 00:16:33 +0200

    fix: rename Firestore collections with _financetracker suffix

--- obj/Debug/net10.0/api.AssemblyInfo.cs
+++ obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c508e1f4bc326bc605fef442312cecb7a587d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d6a5270eaaf38f806339b4a84e26bb7783fbb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
